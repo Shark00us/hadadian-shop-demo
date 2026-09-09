44903869faafce332f36edd190f2ad1a26a1c159
Armin Talakoub <ArminTalakoub@gmail.com>
2026-09-09 14:22:29 +0330

    دشئث

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -78,7 +78,7 @@ function Layout({ children })
                         </div>
 
                         <h3>
-                            آرmin
+                            طلاکوب
                         </h3>
 
                         <p>
